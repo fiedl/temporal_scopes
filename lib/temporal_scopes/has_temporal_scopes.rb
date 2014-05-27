e5da2259786ebbdfffb8152ea6b0cf1dcb7a24cc
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2014-05-27 03:19:51 +0200

    first attempt to implement the temporal scopes.
    Partially, the simple where method is used, partially the more complex
    arel methods, that can be found here:
    
    https://github.com/rails/arel/tree/master/lib/arel

--- lib/temporal_scopes/has_temporal_scopes.rb
+++ lib/temporal_scopes/has_temporal_scopes.rb
@@ -4,8 +4,29 @@ module TemporalScopes
   module HasTemporalScopes
     
     def has_temporal_scopes
+      
+      scope :without_temporal_condition, -> { unscope(where: [:valid_from, :valid_to]) }
+      
+      #scope :now, -> { without_temporal_condition.where('valid_from IS NULL OR valid_from <= ?', Time.zone.now).where('valid_to IS NULL OR valid_to >= ?', Time.zone.now) }
+      scope :now, -> { 
+        without_temporal_condition
+        .where(arel_table[:valid_from].eq(nil).or(arel_table[:valid_from].lteq(Time.zone.now)))
+        .where(arel_table[:valid_to].eq(nil).or(arel_table[:valid_to].gteq(Time.zone.now)))
+      }
+      scope :past, -> { without_temporal_condition.where('valid_to < ?', Time.zone.now) }
+      
+      default_scope { now }
+      
+      extend ClassMethods
+      include InstanceMethods
     end
     
+    module ClassMethods
+    end
+    
+    module InstanceMethods
+    end
+
   end
 end
 
--- spec/temporal_scopes/has_temporal_scopes_spec.rb
+++ spec/temporal_scopes/has_temporal_scopes_spec.rb
@@ -6,4 +6,60 @@ RSpec.describe TemporalScopes::HasTemporalScopes do
     expect(ActiveRecord::Base).to respond_to :has_temporal_scopes
   end
   
+  describe "with a class having temporal scopes" do
+    # 
+    # Here, we use the Article class defined in the dummy app
+    # as an example.
+    # 
+    # class Article < ActiveRecord::Base
+    #   has_temporal_scopes
+    #   
+    # end
+    #
+    subject(:class_with_temporal_scopes) { Article }
+    
+    let(:current_article) { create(:article) }
+    let(:past_article) { create(:past_article) }
+    
+    describe ".all (default scope)" do
+      subject(:default_scope) { class_with_temporal_scopes.all }
+      it { should be_kind_of ActiveRecord::Relation }
+      it "returns current objects" do
+        expect(default_scope).to include current_article
+      end
+      it "returns no past objects" do
+        expect(default_scope).not_to include past_article
+      end
+    end
+    
+    describe ".now" do
+      subject(:now_scope) { class_with_temporal_scopes.now }
+      it { should be_kind_of ActiveRecord::Relation }
+      it "returns current objects" do
+        expect(now_scope).to include current_article
+      end
+      it "returns no past objects" do
+        expect(now_scope).not_to include past_article
+      end
+    end
+    
+    describe ".past" do
+      subject(:past_scope) { class_with_temporal_scopes.past }
+      it { should be_kind_of ActiveRecord::Relation }
+      it "returns past objects" do
+        p past_scope.to_sql
+        expect(past_scope).to include past_article
+      end
+      it "returns no current objects" do
+        expect(past_scope).not_to include current_article
+      end
+    end
+    
+    describe ".with_past"
+    describe "#valid_from"
+    describe "#valid_to"
+    describe "#archive"
+    
+  end
+  
 end
